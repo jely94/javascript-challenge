7bdf4f55c918711bceaad0ed13c340a696e458ad
Jeff Lyng <jely94@icloud.com>
2020-11-09 16:31:30 -0600

    added code

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(data);
+
+//Refactor to use Arrow Functions!
+data.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
