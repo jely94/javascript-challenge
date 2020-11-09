c5045b89770ea08cc66d8f614c769a0d5eb8dc67
Jeff Lyng <jely94@icloud.com>
2020-11-09 17:52:26 -0600

    added code

--- static/js/app.js
+++ static/js/app.js
@@ -16,3 +16,46 @@ data.forEach((ufoReport) => {
     });
   });
 
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Create event handlers 
+button.on("click", runEnter);
+
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    // console.log(inputValue);
+    var filteredData = tableData
+    if (inputValue) {
+        filteredData = tableData.filter(row => row.datetime === inputValue);
+        }
+
+    console.log(filteredData);
+
+    // Clear out table
+    tbody.html("")
+
+    filteredData.forEach((ufoReport) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoReport).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+}
+
+
+
+
+
